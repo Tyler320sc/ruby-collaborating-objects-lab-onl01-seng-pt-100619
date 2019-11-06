4c561c9043ef4b17ec76b7697e9f5c100c1a27ad
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-06 06:03:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     @@all
   end
   
-    def self.new_by_filename(filename)
+  def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
@@ -27,15 +27,5 @@ class Song
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
-  end
-
-  
-  # def self.new_by_filename(filename)
-  #   split_file = filename.split(" - ")
-  #   song_name = split_file[1] #I think it was 1, going by memory though
-  #   new_song = self.new(song_name)
-  #   #now do the same with artist
-  #   #and think about associations
-  #   #also think about what they want you to return at the end
-  # end
+  end 
 end
