98e34433a8643885771259eff74ab52a9f6e2ed8
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-06 06:01:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,12 +17,12 @@ class Song
     @@all
   end
   
-  def self.new_by_filename(filename)
-    split_file = filename.split(" - ")
-    song_name = split_file[1] #I think it was 1, going by memory though
-    new_song = self.new(song_name)
-    #now do the same with artist
-    #and think about associations
-    #also think about what they want you to return at the end
-  end
+  # def self.new_by_filename(filename)
+  #   split_file = filename.split(" - ")
+  #   song_name = split_file[1] #I think it was 1, going by memory though
+  #   new_song = self.new(song_name)
+  #   #now do the same with artist
+  #   #and think about associations
+  #   #also think about what they want you to return at the end
+  # end
 end
