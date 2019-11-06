6c961500a4318f571131bf879b3f2254fc7c29e2
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-06 06:02:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
-end
+
   
   # def self.new_by_filename(filename)
   #   split_file = filename.split(" - ")
